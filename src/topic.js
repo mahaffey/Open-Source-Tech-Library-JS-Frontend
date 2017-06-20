584d3726159af6781fadfa576724685267449a69
mahaffey <ryan.a.mahaffey@gmail.com>
2017-06-20 17:45:57 -0400

    topic classes constructor, etc basic functionaloty need to add the fetch without doing so in console manually

---
+++ src/topic.js
@@ -0,0 +1,49 @@
+class Topic {
+  constructor (data) {
+    this.id = data.id
+    this.pic_url = data.pic_url
+    this.name = data.name
+    this.subtopics = data.subtopics
+    this.created_at = data.created_at
+    this.updated_at = data.updated_at
+  }
+
+  render () {
+    return (`
+      <div class="ui card" id="topic-${this.id}">
+        <div class="image">
+          <img src="${this.pic_url}">
+        </div>
+        <div class="content">
+          <a class="header">${this.name}</a>
+          <div class="meta">
+            <span class="date">${this.subtopics[0].name}</span>
+          </div>
+          <div class="description">
+
+          </div>
+        </div>
+        <div class="extra content">
+          <a>
+            <i class="user icon"></i>
+          </a>
+        </div>
+      </div>
+    `)
+  }
+}
+
+class TopicList {
+  constructor() {
+    this.list = []
+  }
+
+  createTopics (topics) {
+    this.list =  topics.map(topic => new Topic(topic))
+  }
+
+  renderTopics () {
+    document.getElementById('ui-1').innerHTML = this.list.map(topic => topic.render()).join('')
+  }
+
+}
